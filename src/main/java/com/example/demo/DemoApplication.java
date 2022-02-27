30c5275d0816f2e74bf11652fc6abcbc7f24013f
Ashish Ranjan <ashishranjan@walkover.in>
2022-02-27 13:06:08 +0530

    Test Boss

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,24 @@
 package com.example.demo;
 
+import com.example.demo.controller.Student;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication implements CommandLineRunner  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+		Student student = new Student();
+		student.setName("Ashish Ranjan");
+		student.setRoll(45);
+		System.out.println(student);
+
+	}
 }
---
+++ src/main/java/com/example/demo/controller/Student.java
@@ -0,0 +1,11 @@
+package com.example.demo.controller;
+
+import lombok.Data;
+
+@Data
+public class Student {
+
+    private int roll ;
+    private String name ;
+
+}

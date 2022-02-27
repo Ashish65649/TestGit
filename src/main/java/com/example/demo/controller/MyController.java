05bb422a65ab62739d48a53212e575b8844820f4
Ashish Ranjan <ashishranjan@walkover.in>
2022-02-27 10:20:31 +0530

    Initial Commit

---
+++ src/main/java/com/example/demo/controller/MyController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+
+    @GetMapping("/data")
+    public ResponseEntity<String> getData() {
+        return new ResponseEntity<String>("Ashish is my name", HttpStatus.OK);
+    }
+
+}

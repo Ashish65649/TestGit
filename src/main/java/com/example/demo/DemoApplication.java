3011143e29e0d7262aaac7cab5593a4ae2b89005
Ashish Ranjan <ashishranjan@walkover.in>
2022-03-03 09:53:03 +0530

    Answer Repo

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import com.example.demo.controller.Answer;
+import com.example.demo.controller.AnswerRepo;
 import com.example.demo.controller.Question;
 import com.example.demo.controller.Repo;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +18,9 @@ public class DemoApplication implements CommandLineRunner {
 	@Autowired
 	Repo repo;
 
+	@Autowired
+	AnswerRepo answerRepo ;
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
@@ -27,6 +31,8 @@ public class DemoApplication implements CommandLineRunner {
 		Answer answer = new Answer();
 		answer.setMy_ans("Java is language");
 		answer.setPostedBy("Ashish Ranjan");
+
+		this.answerRepo.save(answer);
 
 		Question question = Question.builder().qName("What is Java ?").answer(answer).build();
 		this.repo.save(question);
